90f80c0525abbc65c781f5cd638f1459ea5cad53
yana.svnk <ianasavinkova10@gmail.com>
2022-11-27 21:29:57 +0300

    1-й коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! this is your God speaking!");
+        System.out.println("Hello world!");
     }
 }
